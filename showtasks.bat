5a33e06dd57a2d45e0f97776a4e66c4dce6b8ce8
robert-er <robert.roza.pl@gmail.com>
2020-09-26 16:31:13 +0200

    repair showtasks.bat

--- showtasks.bat
+++ showtasks.bat
@@ -8,7 +8,7 @@ goto fail
 echo.
 echo run browser
 start chrome http://localhost:8080/crud/v1/task/getTasks
-if if "%ERRORLEVEL%" == "0" goto end
+if "%ERRORLEVEL%" == "0" goto end
 echo.
 echo RUN CHROME has errors - breaking work
 goto fail
